675e155e92d70cdbd666a9cc888b29a84f7df439
necrolazy <necrolazy@gmail.com>
2025-09-11 20:46:06 +0300

    Add engine main

---
+++ Universe/src/engine/main.cpp
@@ -0,0 +1,11 @@
+// Copyright: (C) 2022 Vyacheslav Smirnov. All rights reserved.
+
+#include "main.h"
+
+namespace engine
+{
+    int exec()
+    {
+        return 0;
+    }
+}
---
+++ Universe/src/engine/main.h
@@ -0,0 +1,11 @@
+// Copyright: (C) 2022 Vyacheslav Smirnov. All rights reserved.
+
+#ifndef ENGINE_MAIN
+#define ENGINE_MAIN
+
+namespace engine
+{
+    int exec();
+}
+
+#endif
