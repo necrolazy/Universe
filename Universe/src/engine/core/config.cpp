9985e75a1b2373719582bfc41008e242d35f1c2d
SmirnovVyacheslav <necrolazy@gmail.com>
2025-10-16 21:59:47 +0300

    Work on main config

--- Universe/src/engine/core/config.cpp
+++ Universe/src/engine/core/config.cpp
@@ -2,7 +2,31 @@
 
 #include "config.h"
 
+#include "engine/def/impl.h"
+
 namespace engine::core::config
 {
-    //
+    class impl
+    {
+        impl_def
+    };
+
+
+    obj::obj()
+    {
+        //
+    }
+    obj::~obj()
+    {
+        //
+    }
+
+    obj::obj(obj&& src) noexcept
+    {
+        //
+    }
+    obj::obj(const obj& src) noexcept
+    {
+        //
+    }
 }
--- Universe/src/engine/core/config.h
+++ Universe/src/engine/core/config.h
@@ -1,6 +1,11 @@
 // Copyright: (C) 2022 Vyacheslav Smirnov. All rights reserved.
 
+#include "engine/def/obj.h"
+
 namespace engine::core::config
 {
-    //
+    class obj
+    {
+        obj_def
+    };
 }
